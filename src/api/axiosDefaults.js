d770f77a04bbaa6a0181d64eb9980822aca6352f
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-07-23 10:35:37 +0000

    Authentication: access and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://lesson-drf-api-d1c8561611f2.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
